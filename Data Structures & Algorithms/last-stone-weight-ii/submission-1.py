4f85951fab58db036c7135d9ac568f89bef6a301
Punith Basavaraj <punithba@usc.edu>
2026-03-31 16:39:29 -0700

    Add: last-stone-weight-ii - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight-ii/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lastStoneWeightII(self, stones: List[int]) -> int:
+        Stonetotal = sum(stones)
+        target = (Stonetotal+1)//2
+        dp = {}
+
+        def dfs(i, total):
+            if total >= target or i == len(stones):
+                return abs(total - (Stonetotal-total))
+            if (i, total) in dp:
+                return dp[(i, total)]
+
+            dp[(i, total)] = min(dfs(i+1, total),
+                                dfs(i+1, total+stones[i]))
+            return dp[(i, total)]
+
+
+        return dfs(0,0)
